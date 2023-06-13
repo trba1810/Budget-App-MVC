847400e477345e1b6611300d681dc7810ffadf15
trba1810 <trba8l@live.com>
2023-06-13 11:38:52 +0200

    added db connection and ef core nuggets

---
+++ Budget App/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Budget_App.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+
+    }
+}
