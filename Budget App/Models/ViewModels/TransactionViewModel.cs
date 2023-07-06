057bc14f12cb11daabdb47faae8b78bd0d273dc8
trba1810 <trba8l@live.com>
2023-07-06 14:22:32 +0200

    added transaction viewmodel and create modal

---
+++ Budget App/Models/ViewModels/TransactionViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Budget_App.Models.ViewModels
+{
+    public class TransactionViewModel
+    {
+        public List<Transaction> Transactions { get; set; }
+
+        public Transaction Transaction { get; set; }
+    }
+}
